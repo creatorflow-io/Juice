fc6ae1fd5fa2ce1711887ce0eab51d03576ffc59
Jack <jack@creatorflow.io>
2022-11-03 13:46:57 +0700

    Rebase master

--- services/bgservice/src/Juice.BgService/Management/Extensions/ServiceManagerSeviceCollectionExtensions.cs
+++ services/bgservice/src/Juice.BgService/Management/Extensions/ServiceManagerSeviceCollectionExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using Juice.BgService.Management.File;
 using Juice.Extensions.Configuration;
-using Juice.Extensions.DependencyInjection;
+using Juice.Extensions.Options;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
