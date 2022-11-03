f219984f807160a41aae4e8ec8a8e34eeee8576f
Jack <jack@creatorflow.io>
2022-11-03 13:46:57 +0700

    Rebase master

---
+++ services/bgservice/src/Juice.BgService/Extensions/Logging/FileLoggerProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Concurrent;
+using Juice.Extensions.Logging;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Juice.BgService.Extensions.Logging
+{
+    [ProviderAlias("File")]
+    internal class FileLoggerProvider : LoggerProvider
+    {
+        ConcurrentQueue<LogEntry> InfoQueue = new ConcurrentQueue<LogEntry>();
+        public override void WriteLog<TState>(LogEntry<TState> entry)
+        {
+
+        }
+    }
+}
