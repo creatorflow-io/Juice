1ee9af9b3a9503c926810de5a3656f4df9056edb
Jack <jack@creatorflow.io>
2022-12-03 21:05:43 +0700

    Add redis dataprotection, distributedcache samples

--- test/Juice.Tests.Host/Program.cs
+++ test/Juice.Tests.Host/Program.cs
@@ -5,6 +5,9 @@ using Juice.MediatR.RequestManager.EF.DependencyInjection;
 using Juice.MultiTenant;
 using Juice.MultiTenant.DependencyInjection;
 using Juice.Tests.Host;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Caching.Distributed;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +27,10 @@ builder.Services.AddMultiTenant<Tenant>()
 
 ConfigureTenantOptions(builder.Services);
 
+ConfigureDataProtection(builder.Services, builder.Configuration.GetSection("Redis:ConfigurationOptions"));
+
+ConfigureDistributedCache(builder.Services, builder.Configuration);
+
 var app = builder.Build();
 
 app.UseMultiTenant();
@@ -40,8 +47,40 @@ app.UseEndpoints(endpoints =>
         await
            context.Response.WriteAsync("Hello " + (tenant?.Name ?? "Host") + ". Your options name is " + (options.Value?.Name ?? ""));
     });
-});
 
+    endpoints.MapGet("/protect", async (context) =>
+    {
+        var dataProtector = context.RequestServices.GetRequiredService<IDataProtectionProvider>().CreateProtector("abcxyz");
+        var input = "protection data";
+        var protectedPayload = dataProtector.Protect(input);
+        await context.Response.WriteAsync(protectedPayload);
+    });
+
+    endpoints.MapGet("/unprotect", async (context) =>
+    {
+        var dataProtector = context.RequestServices.GetRequiredService<IDataProtectionProvider>().CreateProtector("abcxyz");
+        var input = context.Request.Query["data"].ToString();
+        var unprotectedPayload = dataProtector.Unprotect(input);
+        await context.Response.WriteAsync(unprotectedPayload);
+    });
+
+    endpoints.MapGet("/writecache", async (context) =>
+    {
+        var cache = context.RequestServices.GetRequiredService<IDistributedCache>();
+        var input = context.Request.Query["data"].ToString();
+        await cache.SetStringAsync("cachedKey", input);
+        context.Response.StatusCode = StatusCodes.Status200OK;
+        return;
+    });
+
+    endpoints.MapGet("/readcache", async (context) =>
+    {
+        var cache = context.RequestServices.GetRequiredService<IDistributedCache>();
+        var input = context.Request.Query["data"].ToString();
+        var value = await cache.GetStringAsync("cachedKey");
+        await context.Response.WriteAsync(value);
+    });
+});
 
 app.Run();
 
@@ -51,4 +90,42 @@ static void ConfigureTenantOptions(IServiceCollection services)
     services.AddTenantsConfiguration().AddTenantsJsonFile("appsettings.Development.json");
 
     services.ConfigureTenantsOptions<Options>("Options");
+}
+
+static void ConfigureDataProtection(IServiceCollection services, IConfiguration configuration)
+{
+    //var redis = ConnectionMultiplexer.Connect(new ConfigurationOptions
+    //{
+    //    EndPoints = { "redis-17796.c1.ap-southeast-1-1.ec2.cloud.redislabs.com:17796" },
+    //    Password = "<password>",
+    //    User = "default"
+    //});
+
+    //var db = redis.GetDatabase();
+    //var pong = db.Ping();
+    //Console.WriteLine(pong);
+
+    services.AddDataProtection()
+        .PersistKeysToStackExchangeRedis(() =>
+        {
+            var options = new ConfigurationOptions();
+            configuration.Bind(options);
+            foreach (var endpoint in configuration.GetSection("EndPoints").Get<string[]>())
+            {
+                options.EndPoints.Add(endpoint);
+            }
+            var redis = ConnectionMultiplexer.Connect(options);
+
+            return redis.GetDatabase();
+
+        }, "DataProtection-Keys");
+}
+
+static void ConfigureDistributedCache(IServiceCollection services, IConfiguration configuration)
+{
+    services.AddStackExchangeRedisCache(options =>
+    {
+        options.Configuration = configuration.GetConnectionString("Redis");
+        options.InstanceName = "SampleInstance";
+    });
 }
