29491e7bc0885573ee76e4f5d8b3d56bf89ad1b4
ductran <duc@hdvietnam.com.vn>
2023-09-20 17:20:02 +0700

    Move GenerateStreamFromString to StringUtils

--- services/audit/src/Juice.Audit/Services/GrpcAuditService.cs
+++ services/audit/src/Juice.Audit/Services/GrpcAuditService.cs
@@ -91,13 +91,6 @@ namespace Juice.Audit.Api.Grpc.Services
         }
 
         private static Stream GenerateStreamFromString(string? s)
-        {
-            var stream = new MemoryStream();
-            var writer = new StreamWriter(stream);
-            writer.Write(s);
-            writer.Flush();
-            stream.Position = 0;
-            return stream;
-        }
+            => StringUtils.GenerateStreamFromString(s);
     }
 }
--- services/audit/src/Juice.Audit/StringUtils.cs
+++ services/audit/src/Juice.Audit/StringUtils.cs
@@ -21,5 +21,15 @@ namespace Juice.Audit
                             .Replace("/#", "(\\/[^\\/]+)*")
                             .Replace("#/", "([^\\/]+\\/)*") + "$", RegexOptions.IgnoreCase);
         }
+
+        public static Stream GenerateStreamFromString(string? s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
     }
 }
