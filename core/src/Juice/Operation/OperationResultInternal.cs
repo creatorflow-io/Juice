f26d9e24fbd2407f6ac5b893d4b1f469f43d6554
ductran <ductran.tb@gmail.com>
2024-10-15 09:31:01 +0700

    Improve OperationResult: add Failure and StackTrace

---
+++ core/src/Juice/Operation/OperationResultInternal.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace Juice.Operation
+{
+    internal class OperationResultInternal : IOperationResult
+    {
+        protected string? _message;
+        public string? Message
+        {
+            get { return _message ?? Exception?.InnerException?.Message ?? Exception?.Message; }
+            set
+            {
+                _message = value;
+            }
+        }
+        public string? StackTrace => _trace;
+        public bool Succeeded { get; init; }
+        public OperationalFailure Failure { get; init; }
+
+        [JsonIgnore]
+        private Exception? _exception;
+        public Exception? Exception
+        {
+            get { return _exception; }
+            init
+            {
+                _exception = value;
+                if (Failure == OperationalFailure.None)
+                {
+                    Failure = value is UnauthorizedAccessException ? OperationalFailure.Unauthorized
+                    : value is NotImplementedException ? OperationalFailure.NotImplemented
+                    : Failure;
+                }
+                SetTraceInfo();
+            }
+        }
+
+        public OperationResultInternal()
+        {
+        }
+
+        public OperationResultInternal(OperationalFailure failure, string message)
+        {
+            Succeeded = false;
+            Failure = failure;
+            Message = message;
+            if (failure != OperationalFailure.None)
+            {
+                SetTraceInfo();
+            }
+        }
+
+        protected virtual int StackTraceSkip => 3;
+        private string? _trace;
+        private void SetTraceInfo()
+        {
+            _trace = _exception?.StackTrace
+                ?? new StackTrace(new StackTrace(StackTraceSkip, true).GetFrames().Take(3)).ToString();
+        }
+
+        public void ThrowIfNotSucceeded()
+        {
+            if (!Succeeded)
+            {
+                Exception ex = new OperationException(Message, _exception);
+                if (_trace != null)
+                {
+                    ex = System.Runtime.ExceptionServices.ExceptionDispatchInfo.SetRemoteStackTrace(ex, _trace);
+                }
+                System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw(ex);
+            }
+        }
+
+        protected virtual string FailureMessage => Failure switch
+        {
+            OperationalFailure.NotFound => "Not Found",
+            OperationalFailure.Unauthorized => "Unauthorized",
+            OperationalFailure.NotImplemented => "Not Implemented",
+            _ => "Operation Failed"
+        };
+
+        public override string? ToString()
+            => Message ?? (Succeeded ? "Operation Succeeded" : FailureMessage);
+
+    }
+
+}
