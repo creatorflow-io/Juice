a03d00439daf216f3a2e43144df6c09e4a1053a1
ductran <ductran.tb@gmail.com>
2024-10-29 15:19:39 +0700

    Use System.Text.Json

---
+++ core/src/Juice/Operation/OperationModel.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Juice.Operation
+{
+    public record OperationModel
+    {
+        public string? Message { get; }
+        public string? StackTrace { get; }
+        public bool Succeeded { get; }
+        public OperationalFailure Failure { get; }
+
+        public OperationModel(string? message, string? stackTrace, bool succeeded, OperationalFailure failure)
+        {
+            Message = message;
+            StackTrace = stackTrace;
+            Succeeded = succeeded;
+            Failure = failure;
+        }
+    }
+
+    public record OperationModel<T>:OperationModel
+    {
+        public T? Data { get; }
+
+        public OperationModel(string? message, string? stackTrace, bool succeeded, OperationalFailure failure, T data) : base(message, stackTrace, succeeded, failure)
+        {
+            Data = data;
+        }
+    }
+}
